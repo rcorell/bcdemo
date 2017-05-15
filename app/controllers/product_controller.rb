6130fe9000542ac95ab574adbd6e106c0e12aa48
Rob Corell <rob@robcorell.com>
2017-05-15 03:40:05 -0500

    Add Product creation and listing

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,2 +1,38 @@
 class ProductController < ApplicationController
+
+  def index
+    @products = Product.order('created_at').all
+  end
+
+  def new
+    @product = Product.new( session[:product] )
+    session[:product] = nil
+  end
+
+  def create
+    @product = Product.new( product_params )
+
+    if @product.save
+      flash[:success] = [ "Product \"#{@product.title}\" created successfully." ]
+      redirect_to product_index_path
+    else
+      flash[:error] = @product.errors.full_messages
+      session[:product] = product_params
+      redirect_to new_product_path
+    end
+
+  end
+
+  private
+
+    def product_params
+      params[:product]
+        .permit(
+            :title,
+            :description,
+            :start_date,
+            :end_date,
+            :discontinued)
+    end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
 
   has_many :rate_plans
 
+  validates :title,        length: { minimum: 1, maximum: 255 }
+  validates :description,  length: { minimum: 1, maximum: 255 }
+
 end
