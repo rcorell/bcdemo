c8d67d36d1a71bbecf1705400ae299d41e890f62
Rob Corell <rob@robcorell.com>
2017-05-15 07:54:42 -0500

    Add migration for RatePlan enum change

--- db/migrate/20170515123745_change_rate_plan_type_to_enum.rb
+++ db/migrate/20170515123745_change_rate_plan_type_to_enum.rb
@@ -1,11 +1,11 @@
 class ChangeRatePlanTypeToEnum < ActiveRecord::Migration[5.1]
 
   def up
-		change_column :rate_plan, :type, :integer, default: 0, nil: false
+		change_column :rate_plans, :type, :integer, default: 0, nil: false
   end
 
 	def down
-		change_column :rate_plan, :type, :string
+		change_column :rate_plans, :type, :string
 	end
 
 end
